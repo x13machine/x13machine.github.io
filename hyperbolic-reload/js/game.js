d2fdc5d124272727e19022f47ed1f9e6d436d495
x13machine <brandanw2@gmail.com>
2015-06-23 17:30:03 -0500

    sdfghtsrd

--- hyperbolic-reload/js/game.js
+++ hyperbolic-reload/js/game.js
@@ -11,7 +11,6 @@ var lastFrame=0;
 
 addEventListener("keydown", function (e) {
 	keysDown[e.keyCode] = true;
-	//console.log(e.keyCode)
 }, false);
 
 addEventListener("keyup", function (e) {
@@ -20,18 +19,23 @@ addEventListener("keyup", function (e) {
 
 
 var r = 0.15;
+
+var maxDistance=6
 var player={
 	x:0,
 	y:0,
 	ax:0,
 	ay:0,
-	maxDis:6,
+
 	rotSpeed:Math.TAU/1000,
 	rotation:0,
-	acceleration:0.0001,
+	acceleration:0.00005,
 	maxSpeed:0.1,
 }
-
+var bulletSpeed=0.005
+var bulletRate=1000/10 // 10 hertz
+var bulletLastFire=0;
+var bullets=[]
 var fn = function () {
 	
 	//how much time has pasted since last frame
@@ -42,28 +46,42 @@ var fn = function () {
 
 	if(37 in keysDown || 65 in keysDown)player.rotation+=player.rotSpeed*milliChange//left key or A key
 	if(39 in keysDown || 68 in keysDown)player.rotation-=player.rotSpeed*milliChange//right key or D key
-	if(38 in keysDown){
+
+	if(38 in keysDown || 87 in keysDown){//up key or W key
 		var vec=Point.CENTER.distantPoint(player.acceleration, player.rotation)
 		player.ax+=2 * Math.atanh(vec.x)
 		player.ay+=2 * Math.atanh(vec.y)
 	}
 	
-	if(40 in keysDown){
+	if(40 in keysDown || 83 in keysDown){//down key or S key
 		var vec=Point.CENTER.distantPoint(player.acceleration, player.rotation+Math.PI)
 		player.ax+=2 * Math.atanh(vec.x)
 		player.ay+=2 * Math.atanh(vec.y)
 	}
 	
-	var speed=new Point({
-		x:Math.tanh(player.ax/2),
-		y:Math.tanh(player.ay/2)
-	}).distanceFromCenter();
-	
-	if(speed>player.speed){
-		player.ax=(player.ax/dis)*player.maxSpeed
-		player.ay=(player.ay/dis)*player.maxSpeed
+	if(32 in keysDown){//space bar
+		if(now-bulletLastFire>bulletRate){
+			bulletLastFire=now
+			bullets.push({
+				point:new Point({
+					x:Math.tanh(player.x/2),
+					y:Math.tanh(player.y/2)
+				}),
+				angle:player.rotation
+			})
+		}
 	}
 	
+	//updates bullets
+	for(var i in bullets){
+		bullets[i].point=bullets[i].point.distantPoint(bulletSpeed*milliChange, bullets[i].angle)
+		
+		if(bullets[i].point.distanceFromCenter()>maxDistance){
+			bullets.splice(i, 1);
+		}
+	}
+	
+	//updates player
 	player.x+=player.ax*milliChange
 	player.y+=player.ay*milliChange
 	
@@ -71,21 +89,33 @@ var fn = function () {
 		x:Math.tanh(player.x/2),
 		y:Math.tanh(player.y/2)
 	})
+	
+	//warp the player around
 	var currentDistance = location.distanceFromCenter();
-	if (currentDistance > player.maxDis) {
-
-		var newPoint = Point.givenHyperbolicPolarCoordinates(2 * player.maxDis - currentDistance, player.rotation + Math.PI);
-		player.x=2 * Math.atanh(newPoint.x);
-		player.y=2 * Math.atanh(newPoint.y);
+	if (currentDistance > maxDistance) {
+		/* weird mess
+			// doesn't work
+			var newPoint = Point.givenHyperbolicPolarCoordinates(2 * maxDistance - currentDistance, player.rotation + Math.PI);
+			//console.log(newPoint)
+			player.x=2 * Math.atanh(newPoint.x);
+			player.y=2 * Math.atanh(newPoint.y);
+		*/
+		
+		//this doesn't work either
+		player.x=-player.x
+		player.y=-player.y
 	}
 	
+	//clears screen
 	c.ctx.clearRect(0, 0, c.diameter, c.diameter);
+	
+	
+	//renders player
 	var color='#F00'
 	c.ctx.shadowColor = color;
 	c.ctx.shadowBlur = 40;
 	c.ctx.strokeStyle = color;
 	c.ctx.lineWidth = 2;
-	var polygons = [];
 	var i=0;
 	var j=0;
 	
@@ -104,6 +134,17 @@ var fn = function () {
 	var gon=new Polygon({ vertices: vertices });
 	c.strokePolygon(gon)
 
+	//renders bullets
+	
+	var color='#FF0'
+	c.ctx.shadowColor = color;
+	c.ctx.strokeStyle = color;
+	
+	for(var i in bullets){
+		var p=Polygon.givenNCenterRadius(4,bullets[i].point,0.03,bullets[i].angle+Math.PI/4)
+		c.strokePolygon(p);
+	}
+	
 	requestAnimFrame(fn)
 };
 
